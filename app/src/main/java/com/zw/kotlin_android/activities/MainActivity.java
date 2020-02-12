eed64756f391c1fba2ab0159c900a7d9354e993b
toberole <zhouwei441300@163.com>
2020-02-12 16:31:48 +0800

    init

---
+++ app/src/main/java/com/zw/kotlin_android/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zw.kotlin_android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zw.kotlin_android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
